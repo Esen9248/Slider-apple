19ff87f2624283f441a2bcf4918cdf3510ea299c
Esen Esenaliev <eee.coool@gmail.com>
2017-11-14 21:26:38 +0600

    modified first

--- gulpfile.js
+++ gulpfile.js
@@ -25,9 +25,7 @@ gulp.task('minify-js', function() {
 });
 // Copy lib libraries from /node_modules into /lib
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-     .pipe(gulp.dest('lib/bootstrap'))
-
+	
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('lib/jquery'))
 
